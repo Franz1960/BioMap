d86c3a72d645e8df17d04aedbe4f63650df239af
fh <fh@informeon.de>
2020-10-02 14:22:38 +0000

    Der Ort wird den Elementen beim Migrieren zugeordnet.

--- Data/Place.cs
+++ Data/Place.cs
@@ -8,5 +8,18 @@ namespace BioMap
     public string Name;
     public double Radius = 150;
     public LatLng LatLng;
+    //
+    public static Place GetNearestPlace(LatLng latLng) {
+      Place nearestPlace = null;
+      double minDistance = double.MaxValue;
+      foreach (var p in DataService.Instance.AllPlaces) {
+        var d = GeoCalculator.GetDistance(p.LatLng,latLng);
+        if (nearestPlace==null || d<minDistance) {
+          nearestPlace=p;
+          minDistance=d;
+        }
+      }
+      return nearestPlace;
+    }
   }
 }
