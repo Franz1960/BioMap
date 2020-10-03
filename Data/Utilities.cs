20456d1ae7ec0b1b35177aedf794ca3bf390d1cf
fh <fh@informeon.de>
2020-10-03 04:32:17 +0000

    Platzfilter global implementiert.

---
+++ Data/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BioMap
+{
+  public static class Utilities
+  {
+    public static void FireEvent(EventHandler ev,object sender,object e) {
+      if (ev!=null) {
+        foreach (Delegate d in ev.GetInvocationList()) {
+          d.DynamicInvoke(new object[] { sender,e });
+        }
+      }
+    }
+  }
+}
