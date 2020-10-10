e0865ca9f1e73a3bde11ed3a92543d2ea88fe5a2
fh <fh@informeon.de>
2020-10-10 12:25:55 +0000

    Fitten von Wachstumskurven an Datenreihen implementiert.

--- Data/Utilities.cs
+++ Data/Utilities.cs
@@ -11,5 +11,15 @@ namespace BioMap
         }
       }
     }
+    public static DateTime DateTime_from_Years(double dYears) {
+      int nYears = (int)Math.Floor(dYears);
+      var dt = new DateTime(nYears,1,1);
+      dt+=TimeSpan.FromDays((dYears-nYears)*365);
+      return dt;
+    }
+    public static double Years_from_DateTime(DateTime dt) {
+      double dYears = dt.Year+Math.Min(365,dt.DayOfYear)/365.001;
+      return dYears;
+    }
   }
 }
