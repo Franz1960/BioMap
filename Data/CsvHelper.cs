fee5966454a46f249d91ca9c7c2191df6d0cf1d6
Franz Häring <fh@itools.de>
2020-12-21 16:04:49 +0100

    Temperature and precipitation as line charts in catches over time.

---
+++ Data/CsvHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+
+namespace BioMap
+{
+  public class CsvHelper
+  {
+    public class CsvContent {
+      public class Row {
+        public DateTime DateTime;
+        public double[] Columns;
+      }
+      public string[] Headers;
+      public Row[] Rows;
+    }
+    //
+    public CsvContent ReadCsv(string sFileName) {
+      var ds = DataService.Instance;
+      var lHeaders=new List<string>();
+      var laRows=new List<CsvContent.Row>();
+      var sFilePath=ds.DataDir + "conf/"+sFileName;
+      if (System.IO.File.Exists(sFilePath)) {
+        var sr = new System.IO.StreamReader(sFilePath);
+        try {
+          var sLine = sr.ReadLine();
+          lHeaders.AddRange(sLine.Split(',',StringSplitOptions.None));
+          while (true) {
+            sLine=sr.ReadLine();
+            if (string.IsNullOrEmpty(sLine)) {
+              break;
+            } else {
+              DateTime? dt=null;
+              var lRowColumns=new List<double>();
+              foreach (var sCol in sLine.Split(',')) {
+                if (!dt.HasValue) {
+                  dt=DateTime.Parse(sCol);
+                } else {
+                  lRowColumns.Add(ConvInvar.ToDouble(sCol));
+                }
+              }
+              laRows.Add(new CsvContent.Row {
+                DateTime=dt.Value,
+                Columns=lRowColumns.ToArray(),
+              });
+            }
+          }
+        } finally {
+          sr.Close();
+        }
+      }
+      var csvContent=new CsvContent {
+        Headers=lHeaders.ToArray(),
+        Rows=laRows.ToArray(),
+      };
+      return csvContent;
+    }
+  }
+}
--- Pages/Diagrams/Catches.razor.cs
+++ Pages/Diagrams/Catches.razor.cs
@@ -8,12 +8,14 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using GoogleMapsComponents;
 using ChartJs.Blazor;
+using ChartJs.Blazor.Util;
 using ChartJs.Blazor.Common;
 using ChartJs.Blazor.Common.Enums;
 using ChartJs.Blazor.Common.Axes;
 using ChartJs.Blazor.Common.Axes.Ticks;
 using ChartJs.Blazor.Common.Handlers;
 using ChartJs.Blazor.Common.Time;
+using ChartJs.Blazor.LineChart;
 using ChartJs.Blazor.BarChart;
 using ChartJs.Blazor.BarChart.Axes;
 
@@ -54,19 +56,33 @@ namespace BioMap.Pages.Diagrams
             Display = true,
           },
           Scales = new BarScales {
-            XAxes = new List<CartesianAxis>
-        {
-                new BarCategoryAxis
-                {
+            XAxes = new List<CartesianAxis> {
+                new BarCategoryAxis {
                     Stacked = true
-                }
+                },
             },
-            YAxes = new List<CartesianAxis>
-        {
-                new BarLinearCartesianAxis
-                {
-                    Stacked = true
-                }
+            YAxes = new List<CartesianAxis> {
+                new BarLinearCartesianAxis {
+                  ID="cnt",
+                  ScaleLabel=new ScaleLabel {
+                    Display=true,
+                  },
+                  Stacked = true
+                },
+                new LinearCartesianAxis {
+                  ID="tavg",
+                  ScaleLabel=new ScaleLabel {
+                    LabelString="°C",
+                    Display=true,
+                  },
+                },
+                new LinearCartesianAxis {
+                  ID="prcp",
+                  ScaleLabel=new ScaleLabel {
+                    LabelString="mm",
+                    Display=true,
+                  },
+                },
             }
           },
         },
@@ -143,39 +159,85 @@ namespace BioMap.Pages.Diagrams
         _configOverTime.Data.Labels.Clear();
         _configOverTime.Data.Datasets.Clear();
         //
-        int nIndex = 0;
-        foreach (var indiSpec in new[] {
-          new Tuple<string,Func<Element,bool>>(Localize["Hibernations"]+": 2+",(el)=>el.GetWinters()>=2),
-          new Tuple<string,Func<Element,bool>>(Localize["Hibernations"]+": 1",(el)=>el.GetWinters()==1),
-          new Tuple<string,Func<Element,bool>>(Localize["Hibernations"]+": 0",(el)=>el.GetWinters()==0),
-        }) {
-          var dictCatches = new Dictionary<string,int>();
-          foreach (var ea in aaIndisByIId.Values) {
-            foreach (var el in ea) {
-              if (indiSpec.Item2(el)) {
-                string sKey = el.GetIsoDateTime().Substring(2,5);
-                if (!dictCatches.ContainsKey(sKey)) {
-                  dictCatches[sKey]=0;
+        {
+          int nIndex = 0;
+          foreach (var indiSpec in new[] {
+            new Tuple<string,Func<Element,bool>>(Localize["Hibernations"]+": 2+",(el)=>el.GetWinters()>=2),
+            new Tuple<string,Func<Element,bool>>(Localize["Hibernations"]+": 1",(el)=>el.GetWinters()==1),
+            new Tuple<string,Func<Element,bool>>(Localize["Hibernations"]+": 0",(el)=>el.GetWinters()==0),
+          }) {
+            var dictCatches = new Dictionary<string,int>();
+            foreach (var ea in aaIndisByIId.Values) {
+              foreach (var el in ea) {
+                if (indiSpec.Item2(el)) {
+                  string sKey = el.GetIsoDateTime().Substring(2,5);
+                  if (!dictCatches.ContainsKey(sKey)) {
+                    dictCatches[sKey]=0;
+                  }
+                  dictCatches[sKey]++;
                 }
-                dictCatches[sKey]++;
               }
             }
-          }
-          var ds = new BarDataset<int>() {
-            Label=indiSpec.Item1,
-            BackgroundColor=this.GetColor(nIndex),
-          };
-          var lDates = new List<string>(dictCatches.Keys);
-          lDates.Sort();
-          for (int idx=0;idx<lDates.Count;idx++) {
-            string sKey=lDates[idx];
-            if (nIndex==0) {
-              _configOverTime.Data.Labels.Add(sKey);
+            var ds = new BarDataset<int>() {
+              YAxisId="cnt",
+              Label=indiSpec.Item1,
+              BackgroundColor=this.GetColor(nIndex),
+            };
+            var lDates = new List<string>(dictCatches.Keys);
+            lDates.Sort();
+            for (int idx=0;idx<lDates.Count;idx++) {
+              string sKey=lDates[idx];
+              if (nIndex==0) {
+                _configOverTime.Data.Labels.Add(sKey);
+              }
+              ds.Add(dictCatches[sKey]);
             }
-            ds.Add(dictCatches[sKey]);
+            _configOverTime.Data.Datasets.Add(ds);
+            nIndex++;
           }
-          _configOverTime.Data.Datasets.Add(ds);
-          nIndex++;
+        }
+        {
+          var csvContent = (new CsvHelper()).ReadCsv("MeteoStat Donaustauf.csv");
+          if (csvContent.Rows.Length>=1) {
+            var dsTemperature = new LineDataset<double>() {
+              Label=Localize["Temperature"],
+              BackgroundColor = "rgba(0,0,0,0)",
+              BorderWidth = 2,
+              PointHoverBorderWidth = 0,
+              BorderColor = "Red",
+              PointRadius = 3,
+              YAxisId="tavg",
+            };
+            var dsPrecipitation = new LineDataset<double>() {
+              Label=Localize["Precipitation"],
+              BackgroundColor = "rgba(0,0,0,0)",
+              BorderWidth = 2,
+              PointHoverBorderWidth = 0,
+              BorderColor = "Blue",
+              PointRadius = 3,
+              YAxisId="prcp",
+            };
+            int nColIdx_Temperature=Array.IndexOf(csvContent.Headers,"tavg")-1;
+            int nColIdx_Precipitation=Array.IndexOf(csvContent.Headers,"prcp")-1;
+            foreach (var sLabel in _configOverTime.Data.Labels) {
+              int nYear=2000+ConvInvar.ToInt(sLabel.Substring(0,2));
+              int nMonth=ConvInvar.ToInt(sLabel.Substring(3,2));
+              int nCnt=0;
+              double dSum_Temperature=0;
+              double dSum_Precipitation=0;
+              foreach (var row in csvContent.Rows) {
+                if (row.DateTime.Year==nYear && row.DateTime.Month==nMonth) {
+                  nCnt++;
+                  dSum_Temperature+=row.Columns[nColIdx_Temperature];
+                  dSum_Precipitation+=row.Columns[nColIdx_Precipitation];
+                }
+              }
+              dsTemperature.Add(dSum_Temperature/nCnt);
+              dsPrecipitation.Add(dSum_Precipitation);
+            }
+            _configOverTime.Data.Datasets.Insert(0,dsPrecipitation);
+            _configOverTime.Data.Datasets.Insert(0,dsTemperature);
+          }
         }
       }
       {
@@ -247,11 +309,11 @@ namespace BioMap.Pages.Diagrams
       return _Colors[nIndex % _Colors.Length];
     }
     private string[] _Colors = new string[] {
-      ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.Green),
-      ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.Blue),
-      ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.Cyan),
-      ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.DarkMagenta),
-      ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.Magenta),
+      ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.FromArgb(200,System.Drawing.Color.Green)),
+      ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.FromArgb(200,System.Drawing.Color.Blue)),
+      ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.FromArgb(200,System.Drawing.Color.Cyan)),
+      ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.FromArgb(200,System.Drawing.Color.DarkMagenta)),
+      ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.FromArgb(200,System.Drawing.Color.Magenta)),
     };
   }
 }
