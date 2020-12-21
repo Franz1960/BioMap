a5ce4ec77345d452c5df2f5d7f9aa34d8d9fe8ce
Franz Häring <fh@itools.de>
2020-12-21 13:26:59 +0100

    All diagrams separated into .razor and .cs file.

---
+++ Pages/Diagrams/FrequencyByPlace.razor.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using GoogleMapsComponents;
+using ChartJs.Blazor;
+using ChartJs.Blazor.Common;
+using ChartJs.Blazor.Common.Enums;
+using ChartJs.Blazor.Common.Axes;
+using ChartJs.Blazor.Common.Axes.Ticks;
+using ChartJs.Blazor.Common.Handlers;
+using ChartJs.Blazor.Common.Time;
+using ChartJs.Blazor.LineChart;
+using ChartJs.Blazor.BarChart;
+using ChartJs.Blazor.BarChart.Axes;
+
+namespace BioMap.Pages.Diagrams
+{
+  public partial class FrequencyByPlace : ComponentBase
+  {
+    [Inject]
+    protected DataService DS { get; set; }
+    [Inject]
+    protected SessionData SD { get; set; }
+    //
+    private LineConfig _config;
+    private Chart _chartJs;
+
+    protected override void OnInitialized() {
+      base.OnInitialized();
+      _config = new LineConfig {
+        Options = new LineOptions {
+          Animation=new Animation {
+            Duration=0,
+          },
+          Title = new OptionsTitle {
+            Text="XXX",
+            Display = false,
+          },
+          Legend = new Legend {
+            Display = false,
+          },
+          Scales = new Scales {
+            XAxes = new List<CartesianAxis>
+            {
+            new TimeAxis
+            {
+              ScaleLabel = new ScaleLabel
+              {
+                LabelString = Localize["Time"]
+              },
+              Time = new TimeOptions
+              {
+                Unit = TimeMeasurement.Month,
+                Round = TimeMeasurement.Day,
+                TooltipFormat = "DD.MM.YYYY",
+              },
+            }
+          },
+            YAxes = new List<CartesianAxis>
+            {
+            new CategoryAxis
+            {
+            }
+          },
+          },
+        }
+      };
+      SD.Filters.FilterChanged+=(sender,ev) => {
+        RefreshData();
+        base.InvokeAsync(StateHasChanged);
+      };
+      RefreshData();
+    }
+    private void RefreshData() {
+      var dtProjectStart = DS.ProjectStart;
+      var aIndividuals = DS.GetElements(SD.Filters,"indivdata.iid>=1 AND elements.category=350","elements.creationtime ASC");
+      _config.Data.Datasets.Clear();
+      var countByPlaceAndDate = new Dictionary<string,Dictionary<DateTime,int>>();
+      foreach (var el in aIndividuals) {
+        string sPlace = el.GetPlace()?.Name;
+        var date = el.ElementProp.CreationTime.Date;
+        if (!countByPlaceAndDate.TryGetValue(sPlace,out var countByDate)) {
+          countByDate=new Dictionary<DateTime,int>();
+          countByPlaceAndDate.Add(sPlace,countByDate);
+        }
+        if (!countByDate.ContainsKey(date)) {
+          countByDate[date]=0;
+        }
+        countByDate[date]++;
+      }
+      var lPlaces = new List<string>(countByPlaceAndDate.Keys);
+      lPlaces.Sort();
+      _config.Data.YLabels.Clear();
+      for (int idxPlace = 0;idxPlace<lPlaces.Count;idxPlace++) {
+        string sPlace = lPlaces[idxPlace];
+        _config.Data.YLabels.Add(sPlace);
+        var lDates = new List<DateTime>(countByPlaceAndDate[sPlace].Keys);
+        lDates.Sort();
+        foreach (var date in lDates) {
+          int nRadius = (int)Math.Round(2*Math.Sqrt(countByPlaceAndDate[sPlace][date]));
+          var lineSetCurve = new LineDataset<TimePoint> {
+            BackgroundColor = "rgba(239,209,0,0.8)",
+            BorderWidth = 1,
+            PointHoverBorderWidth = 1,
+            BorderColor = "rgba(239,209,0,0.8)",
+            PointRadius = nRadius,
+            PointHoverRadius = nRadius,
+            PointHitRadius = nRadius,
+            ShowLine = false,
+          };
+          lineSetCurve.Label=sPlace+" "+ConvInvar.ToString(countByPlaceAndDate[sPlace][date])+" "+Localize["Individuals"];
+          lineSetCurve.Add(new TimePoint(date,idxPlace));
+          if (lineSetCurve.Data.Count>=1) {
+            _config.Data.Datasets.Add(lineSetCurve);
+          }
+        }
+      }
+    }
+  }
+}
---
+++ Pages/Diagrams/Summary.razor.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using GoogleMapsComponents;
+using ChartJs.Blazor;
+using ChartJs.Blazor.Common;
+using ChartJs.Blazor.Common.Enums;
+using ChartJs.Blazor.Common.Axes;
+using ChartJs.Blazor.Common.Axes.Ticks;
+using ChartJs.Blazor.Common.Handlers;
+using ChartJs.Blazor.Common.Time;
+using ChartJs.Blazor.BarChart;
+using ChartJs.Blazor.BarChart.Axes;
+
+namespace BioMap.Pages.Diagrams
+{
+  public partial class Summary : ComponentBase
+  {
+    [Inject]
+    protected DataService DS { get; set; }
+    [Inject]
+    protected SessionData SD { get; set; }
+    //
+    private int ProjectYearBegin;
+    private int ProjectYearEnd;
+    private class DataByYears_t
+    {
+      public string Name;
+      public string AllYears;
+      public string[] ByYear;
+    }
+    private BarConfig _config;
+    private Chart _chartJs;
+    private readonly List<DataByYears_t> IndividualData = new List<DataByYears_t>();
+    private readonly List<DataByYears_t> OtherData = new List<DataByYears_t>();
+    protected override void OnInitialized() {
+      base.OnInitialized();
+      ProjectYearBegin=DS.ProjectStart.Year;
+      ProjectYearEnd=Math.Max(2024,DateTime.Now.Year);
+      _config = new BarConfig {
+        Options = new BarOptions {
+          Animation=new Animation {
+            Duration=0,
+          },
+          Title = new OptionsTitle {
+            Text="XXX",
+            Display = false,
+          },
+          Legend = new Legend {
+            Display = true,
+          },
+          Scales = new BarScales {
+            XAxes = new List<CartesianAxis>
+            {
+              new BarCategoryAxis
+              {
+                  Stacked = true
+              }
+          },
+            YAxes = new List<CartesianAxis>
+            {
+              new BarLinearCartesianAxis
+              {
+                  Stacked = true
+              }
+          }
+          },
+        },
+      };
+      SD.Filters.FilterChanged+=(sender,ev) => {
+        RefreshData();
+        base.InvokeAsync(StateHasChanged);
+      };
+      RefreshData();
+    }
+    private void RefreshData() {
+      //
+      _config.Data.Datasets.Clear();
+      for (int year = ProjectYearBegin;year<=ProjectYearEnd;year++) {
+        _config.Data.Labels.Add(ConvInvar.ToString(year));
+      }
+      //
+      var aaIndisByIId = DS.GetIndividuals();
+      int nIndex = 0;
+      foreach (var indiSpec in new[] {
+      new Tuple<string,Func<List<Element>,int,bool>,int>(Localize["Known individuals"],(ea,year)=>ea.Any(el=>el.ElementProp.CreationTime.Year<year) && ea[ea.Count-1].ElementProp.CreationTime.Year>=year,1),
+      new Tuple<string,Func<List<Element>,int,bool>,int>(Localize["New individuals"]+" 1+ "+Localize["Hibernations"],(ea,year)=>ea[0].ElementProp.CreationTime.Year==year && ea[0].GetWinters()>=1,1),
+      new Tuple<string,Func<List<Element>,int,bool>,int>(Localize["New individuals"]+" 0 "+Localize["Hibernations"],(ea,year)=>ea[0].ElementProp.CreationTime.Year==year && ea[0].GetWinters()<1,1),
+      new Tuple<string,Func<List<Element>,int,bool>,int>(Localize["Missed individuals"]+" 1+ "+Localize["Hibernations"],(ea,year)=>ea[ea.Count-1].ElementProp.CreationTime.Year==year-1 && ea[ea.Count-1].GetWinters()>=1,-1),
+      new Tuple<string,Func<List<Element>,int,bool>,int>(Localize["Missed individuals"]+" 0 "+Localize["Hibernations"],(ea,year)=>ea[ea.Count-1].ElementProp.CreationTime.Year==year-1 && ea[ea.Count-1].GetWinters()<1,-1),
+    }) {
+        var ds = new BarDataset<int>() { Label=indiSpec.Item1,BackgroundColor=this.GetColor(nIndex) };
+        for (int year = ProjectYearBegin;year<=ProjectYearEnd;year++) {
+          int nCount = 0;
+          foreach (var ea in aaIndisByIId.Values) {
+            if (indiSpec.Item2(ea,year)) {
+              nCount++;
+            }
+          }
+          ds.Add(nCount*indiSpec.Item3);
+        }
+        _config.Data.Datasets.Add(ds);
+        nIndex++;
+      }
+    }
+    public string GetColor(int nIndex) {
+      return _Colors[nIndex % _Colors.Length];
+    }
+    private string[] _Colors = new string[] {
+    ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.Green),
+    ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.Blue),
+    ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.Cyan),
+    ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.DarkMagenta),
+    ChartJs.Blazor.Util.ColorUtil.FromDrawingColor(System.Drawing.Color.Magenta),
+  };
+  }
+}
