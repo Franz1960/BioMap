94a750ecae23b32db5e60b8944b67edeb3e97bc3
Franz Häring <fh@itools.de>
2021-01-09 09:35:06 +0100

    Run Docker detached.

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,4 +3,4 @@
 # Build a Docker image to build and run the BioMap app.
 #
 docker build -t biomap .
-docker run -it --rm -p 5010:80 --mount type=bind,source="/var/www/data",target=/var/www/data --name biomap_run biomap
+docker run -d --rm -p 5010:80 --mount type=bind,source="/var/www/data",target=/var/www/data --name biomap_run biomap
