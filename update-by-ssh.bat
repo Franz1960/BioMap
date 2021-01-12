f6504c8101ad321fb8ba43da0b602439fcda36d3
Franz Häring <fh@itools.de>
2021-01-12 16:01:26 +0100

    AOI ist editierbar.

--- update-by-ssh.bat
+++ update-by-ssh.bat
@@ -1,1 +1,1 @@
-ssh itools.de -l fh "cd \"/var/www/docker/BioMap\" && sudo git pull && sudo ./build-and-run.sh"
+ssh itools.de -l fh "cd \"/var/www/docker/BioMap\" && sudo git pull && sudo chmod +x *.sh && sudo ./build-and-run.sh"
