01145835de007ddc8cf33e3ef7afb15d8674f038
Franz Häring <fh@itools.de>
2021-02-19 17:45:23 +0100

    Dokumentiert.

--- BioMap/ImageProc/SafeCropExtensions.cs
+++ BioMap/ImageProc/SafeCropExtensions.cs
@@ -7,18 +7,18 @@ using System.Numerics;
 namespace BioMap.ImageProc
 {
   /// <summary>
-  /// Defines extension methods to apply binary thresholding on an <see cref="Image"/>
-  /// using Mutate/Clone.
+  /// Defines extension methods to apply safe cropping on an <see cref="Image"/>
+  /// using Mutate/Clone. The base Crop operation throws an exception if a cropped dimension is bigger 
+  /// than the image dimension.
   /// </summary>
   public static class SafeCropExtensions
   {
     /// <summary>
-    /// Applies binarization to the image splitting the pixels at the given threshold with
-    /// Luminance as color component to be compared to threshold.
+    /// Crops an image to the given width and height. If either is bigger than the image dimension, the space is filled with black.
     /// </summary>
     /// <param name="source">The image this method extends.</param>
-    /// <param name="threshold">The threshold to apply binarization of the image. Must be between 0 and 1.</param>
-    /// <param name="hueIntervals">Permissible hue value intervals as array of 2D vectors. X is lower, Y upper interval bounding. Range is circular 0..360.</param>
+    /// <param name="width">The width of the cropped image.</param>
+    /// <param name="height">The height of the cropped image.</param>
     /// <returns>The <see cref="IImageProcessingContext"/> to allow chaining of operations.</returns>
     public static IImageProcessingContext SafeCrop(this IImageProcessingContext source,int width,int height) {
       var sz = source.GetCurrentSize();
@@ -30,6 +30,5 @@ namespace BioMap.ImageProc
       }
       return source.Crop(width,height);
     }
-
   }
 }
