6b168fc83eb484cc87e912c5d55b9a9b75fb5e65
Franz Häring <fh@itools.de>
2021-02-19 08:35:52 +0100

    Remove container before restarting.

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,5 +3,6 @@
 # Build a Docker image to build and run the BioMap app.
 #
 docker container stop biomap_run
+docker container rm biomap_run
 docker build -t biomap .
 docker run -d --restart unless-stopped  -p 5010:80 -p5011:22 --mount type=bind,source="/var/www/data",target=/var/www/data --name biomap_run biomap
