a3541b53a97948e9bc9a952205d455efa2fb4291
Franz Häring <fh@itools.de>
2021-02-20 11:43:31 +0100

    Test.

--- BioMap/Pages/Workflow/NewElements.razor.cs
+++ BioMap/Pages/Workflow/NewElements.razor.cs
@@ -295,7 +295,7 @@ namespace BioMap.Pages.Workflow
             var mPattern = md.GetPatternMatrix(nHeight);
             var atb = new AffineTransformBuilder();
             atb.AppendMatrix(mPattern);
-            imgSrc.Mutate(x => x.Transform(atb));
+            //imgSrc.Mutate(x => x.Transform(atb));
             //imgSrc.Mutate(x => x.SafeCrop(nWidth,nHeight));
             //imgSrc.Mutate(x => x.MaxChroma(0.05f,new[] { new System.Numerics.Vector2(1,100) }));
             //imgSrc.Mutate(x => x.ApplyProcessor(analyseYellowShare));
