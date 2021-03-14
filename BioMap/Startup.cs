15bb965aaf32f2bb8cf25ac572ca473eb378edf5
Franz Häring <fh@itools.de>
2021-03-14 18:53:21 +0100

    .gitattributes und .editorconfig aus ImageSharp übernommen.

--- BioMap/Startup.cs
+++ BioMap/Startup.cs
@@ -18,80 +18,80 @@ using Blazorise.Icons.FontAwesome;
 
 namespace BioMap
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddRazorPages();
-      services.AddServerSideBlazor();
-      services.AddSingleton<DataService>();
-      services.AddScoped<SessionData>();
-      services.AddLocalization(options => options.ResourcesPath = "Resources");
-      services.Configure<RequestLocalizationOptions>(options =>
+        public Startup(IConfiguration configuration)
         {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<DataService>();
+            services.AddScoped<SessionData>();
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.Configure<RequestLocalizationOptions>(options =>
+              {
             // Define the list of cultures your app will support
             var supportedCultures = new List<CultureInfo>()
-            {
+                  {
                 new CultureInfo("en"),
                 new CultureInfo("de")
-            };
+                  };
             // Set the default culture
             options.DefaultRequestCulture = new RequestCulture("en");
-            options.SupportedCultures = supportedCultures;
-            options.SupportedUICultures = supportedCultures;
-        });
-      services.AddControllers();
-      services
-      .AddBlazorise(options =>
-       {
-         options.ChangeTextOnKeyPress = false; // optional
-         options.DelayTextOnKeyPress = true;
-         options.DelayTextOnKeyPressInterval = 500;
-       })
-      .AddBootstrapProviders()
-      .AddFontAwesomeIcons();
+                  options.SupportedCultures = supportedCultures;
+                  options.SupportedUICultures = supportedCultures;
+              });
+            services.AddControllers();
+            services
+            .AddBlazorise(options =>
+             {
+                 options.ChangeTextOnKeyPress = false; // optional
+                 options.DelayTextOnKeyPress = true;
+                 options.DelayTextOnKeyPressInterval = 500;
+             })
+            .AddBootstrapProviders()
+            .AddFontAwesomeIcons();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseRequestLocalization(app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>().Value);
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+          //
+          endpoints.MapControllers();
+            });
+
+            var ds = app.ApplicationServices.GetRequiredService<DataService>();
+            ds.Init();
+        }
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-      }
-
-      //app.UseHttpsRedirection();
-      app.UseStaticFiles();
-
-      app.UseRouting();
-
-      app.UseRequestLocalization(app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>().Value);
-
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapBlazorHub();
-        endpoints.MapFallbackToPage("/_Host");
-        //
-        endpoints.MapControllers();
-      });
-
-      var ds = app.ApplicationServices.GetRequiredService<DataService>();
-      ds.Init();
-    }
-  }
 }
