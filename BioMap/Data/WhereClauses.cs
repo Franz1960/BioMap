327f4a74ae9c4976092219d3e1f1c1e1f6ebefaf
Franz Häring <fh@itools.de>
2021-04-05 18:04:15 +0200

    Monitoring-Kalender (nur Anzeige) implementiert.

--- BioMap/Data/WhereClauses.cs
+++ BioMap/Data/WhereClauses.cs
@@ -12,5 +12,9 @@ namespace BioMap
     {
         public static readonly string Is_ID_photo = "(elements.classification LIKE '%\"ClassName\":\"ID photo\"%')";
         public static readonly string Is_Individuum = Is_ID_photo + " AND indivdata.iid>=1";
+        public static string Is_FromPlace(string sPlaceName)
+        {
+            return "elements.place='" + sPlaceName + "'";
+        }
     }
 }
