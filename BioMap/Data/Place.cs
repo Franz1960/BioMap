7fc2f7c993181c21ddd3af3154f97b3a35c27acc
Franz Häring <fh@itools.de>
2021-05-14 08:41:25 +0200

    Formale Änderung (Code Style).

--- BioMap/Data/Place.cs
+++ BioMap/Data/Place.cs
@@ -1,38 +1,44 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace BioMap
 {
-  [JsonObject(MemberSerialization.Fields)]
-  public class Place : ICloneable
-  {
-    public Place() {
-      this.TraitValues=new List<int>(new int[Places.Traits.Length]);
-    }
-    public string Name;
-    public double Radius = 150;
-    public LatLng LatLng;
-    //
-    public static Place GetNearestPlace(SessionData sd,LatLng latLng,float fDistanceTolerance=0.20f) {
-      Place nearestPlace = null;
-      double minDistance = double.MaxValue;
-      foreach (var p in DataService.Instance.GetPlaces(sd)) {
-        var d = GeoCalculator.GetDistance(p.LatLng,latLng);
-        if (nearestPlace==null || d<minDistance) {
-          if (d<=p.Radius*(1+fDistanceTolerance)) {
-            nearestPlace=p;
-            minDistance=d;
-          }
+    [JsonObject(MemberSerialization.Fields)]
+    public class Place : ICloneable
+    {
+        public Place()
+        {
+            this.TraitValues = new List<int>(new int[Places.Traits.Length]);
         }
-      }
-      return nearestPlace;
-    }
+        public string Name;
+        public double Radius = 150;
+        public LatLng LatLng;
+        //
+        public static Place GetNearestPlace(SessionData sd, LatLng latLng, float fDistanceTolerance = 0.20f)
+        {
+            Place nearestPlace = null;
+            double minDistance = double.MaxValue;
+            foreach (var p in DataService.Instance.GetPlaces(sd))
+            {
+                var d = GeoCalculator.GetDistance(p.LatLng, latLng);
+                if (nearestPlace == null || d < minDistance)
+                {
+                    if (d <= p.Radius * (1 + fDistanceTolerance))
+                    {
+                        nearestPlace = p;
+                        minDistance = d;
+                    }
+                }
+            }
+            return nearestPlace;
+        }
 
-    public object Clone() {
-      return this.MemberwiseClone();
-    }
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
 
-    public readonly List<int> TraitValues = new List<int>();
-  }
+        public readonly List<int> TraitValues = new List<int>();
+    }
 }
