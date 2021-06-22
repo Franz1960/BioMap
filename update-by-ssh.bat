5d20465f805544fdb917b8300b3f7d9ec507eed8
Franz Häring <fh@itools.de>
2021-06-22 11:54:00 +0200

    Temporäre Docker-Images werden gelöscht.

--- update-by-ssh.bat
+++ update-by-ssh.bat
@@ -1,1 +1,3 @@
-ssh itools.de -l fh "cd \"/var/www/docker/BioMap\" && sudo git pull && sudo chmod +x *.sh && sudo ./build-and-run.sh && sudo docker rmi $(docker images --filter "dangling=true" -q --no-trunc)"
+ssh itools.de -l fh "cd \"/var/www/docker/BioMap\" && sudo git pull && sudo chmod +x *.sh && sudo ./build-and-run.sh"
+REM Mit folgendem Kommando können temporäre Container gelöscht werden:
+REM docker rmi $(docker images --filter \"dangling=true\" -q --no-trunc)
