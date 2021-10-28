778874243c0220174346bfe73545e9b7275845ae
Franz Häring <fh@itools.de>
2021-10-28 17:19:37 +0200

    Filter Lebewesen wird in Baumstruktur aufgebaut. Wegen Bug in Blazorise geht Bedienung mit Maus noch nicht.

--- BioMap/Shared/ClassFilter.razor.cs
+++ BioMap/Shared/ClassFilter.razor.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Blazorise;
+using Blazorise.Extensions;
 using GoogleMapsComponents;
 using GoogleMapsComponents.Maps;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.JSInterop;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -24,6 +26,59 @@ namespace BioMap.Shared
     }
     private void Update() {
       SD.Filters.ClassFilter = JsonConvert.SerializeObject(this.Classification);
+    }
+    private RenderFragment CreateTreeNodeMenuItem(TreeNode treeNode) {
+      return builder => {
+        var taxon = treeNode.Data as Taxon;
+        if (treeNode.HasChildren) {
+          builder.OpenComponent<Blazorise.DropdownItem>();
+          builder.Attribute("ChildContent", (RenderFragment)((builder2) => {
+            builder2.OpenComponent<Blazorise.Dropdown>();
+            //builder2.Attribute("Direction", Direction.Right);
+            builder2.Attribute("ChildContent", (RenderFragment)((builder3) => {
+              builder3.OpenComponent<Blazorise.Button>();
+              builder3.Attribute("Color", (string.CompareOrdinal(this.Classification.LivingBeing?.Taxon?.SciName, taxon.InvariantName) == 0) ? Color.Primary : Color.None);
+              builder3.Attribute("Clicked", Microsoft.AspNetCore.Components.EventCallback.Factory.Create(this, () => {
+                this.Classification.LivingBeing = new ElementClassification.LivingBeing_t {
+                  Taxon = this.SD.CurrentProject.GetTaxon(taxon.InvariantName),
+                  Stadium = ElementClassification.Stadium.None,
+                };
+                this.Update();
+              }));
+              builder3.Attribute("ChildContent", (RenderFragment)((builder4) => {
+                builder4.AddContent(45, taxon.GetLocalizedName(this.SD.CurrentCultureName));
+              }));
+              builder3.CloseComponent();
+              builder3.OpenComponent<Blazorise.DropdownToggle>();
+              builder3.Attribute("Split", true);
+              builder3.CloseComponent();
+              builder3.OpenComponent<Blazorise.DropdownMenu>();
+              builder3.Attribute("ChildContent", (RenderFragment)((builder4) => {
+                foreach (var childNode in treeNode.Children) {
+                  builder4.Content(this.CreateTreeNodeMenuItem(childNode));
+                }
+              }));
+              builder3.CloseComponent();
+            }));
+            builder2.CloseComponent();
+          }));
+          builder.CloseComponent();
+        } else {
+          builder.OpenComponent<Blazorise.DropdownItem>();
+          builder.Attribute("Active", (string.CompareOrdinal(this.Classification.LivingBeing?.Taxon?.SciName, taxon.InvariantName) == 0));
+          builder.Attribute("Clicked", Microsoft.AspNetCore.Components.EventCallback.Factory.Create<object>(this, (ea) => {
+            this.Classification.LivingBeing = new ElementClassification.LivingBeing_t {
+              Taxon = this.SD.CurrentProject.GetTaxon(taxon.InvariantName),
+              Stadium = ElementClassification.Stadium.None,
+            };
+            this.Update();
+          }));
+          builder.Attribute("ChildContent", (RenderFragment)((builder2) => {
+            builder2.AddContent(8, taxon.GetLocalizedName(this.SD.CurrentCultureName));
+          }));
+          builder.CloseComponent();
+        }
+      };
     }
   }
 }
